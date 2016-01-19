2ea0a5c4a00f0a5518d8e959487c48c2db366c30
xsy <shenyushunlive@gmail.com>
2016-01-19 16:39:46 +0800

    add dict

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # py-webscanner
 练手写的网站后台扫描器，字典在御剑基础上新增了一些，共1097802条不重复数据。
+
+# 依赖
+pip install requests
---
+++ scanner.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+'''
+@Author：XingSongYan
+@CreateDate： Tue Jan 19 16:32:19 HKT 2016
+@FileName：
+@Description：个人练手写的网站后台扫描器
+'''
+
+
+
+
+if __name__ == "__main__":
+    pass
+
